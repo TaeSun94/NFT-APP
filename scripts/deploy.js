772c4121da447c3acc43629200dde583ada96ee5
Taesun 94 <tyzlddy@naver.com>
2022-01-28 16:21:16 +0900

    Mod: MsgToken deploy js file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,17 @@
 async function main() {
-    //ContractFactory는 새로운 스마트 컨트랙트 배포에 사용
-    const MSGN = await ethers.getContractFactory("MSGN")
+  //ContractFactory는 새로운 스마트 컨트랙트 배포에 사용
+  const MsgNFT = await ethers.getContractFactory("msgToken")
   
-    // Start deployment, returning a promise that resolves to a contract object
-    const myNFT = await MSGN.deploy()
-    await myNFT.deployed()
-    console.log("Contract deployed to address:", myNFT.address)
-  }
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MsgNFT.deploy()
+  await myNFT.deployed()
+  console.log("Contract deployed to address:", myNFT.address)
+}
   
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error)
-      process.exit(1)
-    })
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
   
