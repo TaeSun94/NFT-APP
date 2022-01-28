f3223dd9dc0efc97e5aebfe7ed7d2892091b9017
Taesun 94 <tyzlddy@naver.com>
2022-01-28 16:23:17 +0900

    Add: etherscan api keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,26 +3,28 @@
  */
  require('dotenv').config();
  require("@nomiclabs/hardhat-ethers");
- const { API_URL, PRIVATE_KEY } = process.env;
+ require("@nomiclabs/hardhat-etherscan");
+ const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
  module.exports = {
-    solidity: "0.8.0",
+    solidity: "0.8.2",
     defaultNetwork: "ropsten",
     networks: {
-       hardhat: {},
-       rinkeby: {
+      rinkeby: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       },
-       ropsten: {
-          url: API_URL,
-          accounts: [`0x${PRIVATE_KEY}`]
-       }
-       /*
-       다른 네트워크 추가
-       네트워크이름: {
+         /*
+         다른 네트워크 추가
+         네트워크이름: {
             url: 'DApp api url',
             accouns: ['Contract owner account's Private Key']
-       }
-       */
-    },
+         }
+         */
+   },
+   etherscan:{
+      apiKey: ETHERSCAN_API_KEY,
+      // apiKey: {
+      //    rinkeby: "UWT93ZDS2D4F23IJ92K9ZRTCX66CYB5MKB",
+      // }
+   },
  }
