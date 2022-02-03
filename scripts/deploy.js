614a68daaeedda61326dfda28b66dbb09d2049d4
Taesun 94 <tyzlddy@naver.com>
2022-02-03 18:08:14 +0900

    Mod: ERC721 deploy -> ERC20 deploy module

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,27 @@
+// async function main() {
+//   //ContractFactory는 새로운 스마트 컨트랙트 배포에 사용
+//   const MsgNFT = await ethers.getContractFactory("msgToken")
+  
+//   // Start deployment, returning a promise that resolves to a contract object
+//   const myNFT = await MsgNFT.deploy()
+//   await myNFT.deployed()
+//   console.log("Contract deployed to address:", myNFT.address)
+// }
+  
+// main()
+// .then(() => process.exit(0))
+// .catch((error) => {
+//   console.error(error)
+//   process.exit(1)
+// })
 async function main() {
   //ContractFactory는 새로운 스마트 컨트랙트 배포에 사용
-  const MsgNFT = await ethers.getContractFactory("msgToken")
+  const Safe = await ethers.getContractFactory("SafeStorage")
   
   // Start deployment, returning a promise that resolves to a contract object
-  const myNFT = await MsgNFT.deploy()
-  await myNFT.deployed()
-  console.log("Contract deployed to address:", myNFT.address)
+  const storage = await Safe.deploy()
+  await storage.deployed()
+  console.log("Contract deployed to address:", storage.address)
 }
   
 main()
@@ -14,4 +30,3 @@ main()
   console.error(error)
   process.exit(1)
 })
-  
