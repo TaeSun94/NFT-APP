7babdc14eabc17cad286a6801d69a9aa975a374d
Taesun 94 <tyzlddy@naver.com>
2022-02-17 18:33:33 +0900

    Mod: mutitoken contract creatFT using metadata uri

--- contracts/MultiToken.sol
+++ contracts/MultiToken.sol
@@ -3,59 +3,77 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-contract MultiToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
-    //FT Token
-    mapping (uint256 => mapping(address => uint256)) public balances;
+import "./SafeMath.sol";
+
+contract MultiToken is ERC1155, Ownable, ERC1155Supply {
+    using SafeMath for uint256;
+
+    //Divide for FT & NFT
+    uint256 public divideNum = 100000000;
+    
+    //Metadata uri storage
+    mapping (uint256 => string) public _uri;
+    //FT Token Name
+    mapping (uint256 => string) public tokenNames;
     //For Air Drop NFT Token
     mapping (uint256 => mapping (address => uint256)) public claims;
-    
-    // string contractURI;
+    //MerkleTree rootHash for whitelist
+    bytes32[] public merkleTreeRoot;
+    string public baseURI = "ipfs://";
+    event Mint(address account, string tokenURI);
 
     constructor() ERC1155("") {}
 
-    // function setContractURI(string memory _uri) public onlyOwner {
-    //     contractURI = _uri;
-    // }
-
-    function pause() public onlyOwner {
-        _pause();
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
+    function setFTName (uint256 id, string memory name)
+        internal
+        onlyOwner
+        returns(string memory)
+    {
+        tokenNames[id] = name;
+        return name;
     }
 
-    function createFT(address account, uint256 id, uint256 amount, bytes memory data)
+    function createFT(string memory uri_, uint256 id, uint256 amount, bytes memory data, string memory name)
         public
         onlyOwner
-        whenNotPaused
     {
-        _mint(account, id, amount, data);
+        require(id <= divideNum, "Full FT List! Can't create new Token");
+        _uri[id] = uri_;
+        tokenNames[id] = name;
+        _mint(msg.sender, id, amount.mul(10**18),data);
+        emit URI(uri_, id);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    function createBatchFT(uint256 tokenSize, string[] memory name, uint256[] memory amounts, bytes memory data)
         public
         onlyOwner
-        whenNotPaused
     {
-        _mint(account, id, amount, data);
+        uint[] memory ids = new uint[](amounts.length);
+        for(uint i = 0; i < name.length; i++){
+            ids[i] = tokenSize;
+            tokenNames[tokenSize++] = name[i];
+        }
+        _mintBatch(msg.sender, ids, amounts, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    function mintNFT(string memory uri_, uint256 id, uint256 amount, bytes memory data)
         public
         onlyOwner
-        whenNotPaused
     {
-        _mintBatch(to, ids, amounts, data);
+        _uri[id] = uri_;
+        _mint(msg.sender, id, amount, data);
+        emit URI(uri_, id);
+        emit Mint(msg.sender, uri_);
     }
 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
-        whenNotPaused
         override(ERC1155, ERC1155Supply)
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
